eb16e38f684de07388f11e6a960fef8d034b170a
sleepy9487 <sleepy9487>
2021-11-25 21:08:42 +0800

    1

--- findIndex.js
+++
@@ -1,7 +0,0 @@
-export function findIndex(array, types){
-    for(let i=0; i < array.length; i++){
-        if(array[i].user == types.user && array[i].active == types.active){
-            return i
-        }
-    } 
-}
--- mean_test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'https://deno.land/x/bdd/mod.ts'
-import * as _ from "./mean.ts";
-
-Deno.test("mean", () => {
-    expect(_.mean([4,2,8,6])).to.equal(5)
-  })
-  
