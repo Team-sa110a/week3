7ba1822754572f7549bf1fccbd0f5568d011addb
Yongsin0 <qwer78942506@gmail.com>
2021-12-23 20:30:01 +0800

    123

--- test2/test/isempty_test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "https://deno.land/x/tdd/mod.ts";
-import * as _ from "../src/lodash.ts";
-
-Deno.test("isEmpty", () => {
-  var test = _.isEmpty(1);
-  expect(test).to.equal(true);
-});
