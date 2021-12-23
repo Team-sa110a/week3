da299515ac83c728d82185d37934709917458baa
hongyu101094 <hongyushi101094aaaa@gmail.com>
2021-12-23 20:17:18 +0800

    12/23

--- test1/fill.js
+++
@@ -1,9 +0,0 @@
-/* Fills elements of array with value from start up to, but not including, end.*/
-
-export function fill1(array,value,startnumber,endnumber){ 
-    let ary = []
-    for(let i = 0;i < array.length; i++){
-         ary = (array.fill(value,startnumber,endnumber))
-    }
-    return ary
-}
